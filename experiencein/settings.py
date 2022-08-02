22109f0e5e80e96729e0596d90593d4b6f4f6986
davirsreis <daviresende.2312@gmail.com>
2022-08-01 22:37:29 -0300

    commit

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 LOGIN_URL="/login/"
 LOGOUT_URL="/logout/"
